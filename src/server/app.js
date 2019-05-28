d92e4def5901492c10ac9172460ae811a70d33c8
Adetunji Femi <phemmzmcllroy@gmail.com>
2019-05-28 03:46:12 +0100

    feat(generate-numbers): - create new post route for generating numbers - create controller for generating phone numbers and saving in a file - write tests

--- src/server/app.js
+++ src/server/app.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import bodyparser from 'body-parser';
 
+import routes from './routes';
+
 const app = express();
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 
+app.use('/api/v1', routes);
+
 const { PORT = 3000 } = process.env;
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+
+export default app;
